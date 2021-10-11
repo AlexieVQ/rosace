acb5d1d3a22b56daa97fc35d6867e4a08bd6180b
AlexieVQ <alexie@nephos.me>
2021-10-11 21:29:51 +0200

    Added require_relative

--- lib/rosace.rb
+++ lib/rosace.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "rosace/version"
+require_relative "rosace/context"
+require_relative "rosace/contextual_value"
+require_relative "rosace/entity"
+require_relative "rosace/function"
 
 module Rosace
   class Error < StandardError; end
