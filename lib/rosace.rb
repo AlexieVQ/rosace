361193652e2b6364f8c8e89201e50e5f85e051ec
AlexieVQ <alexie@nephos.me>
2021-10-12 10:56:30 +0200

    EvaluationException added to rosace.rb

--- lib/rosace.rb
+++ lib/rosace.rb
@@ -5,6 +5,7 @@ require_relative "rosace/context"
 require_relative "rosace/contextual_value"
 require_relative "rosace/entity"
 require_relative "rosace/function"
+require_relative "rosace/evaluation_exception"
 
 module Rosace
   class Error < StandardError; end
